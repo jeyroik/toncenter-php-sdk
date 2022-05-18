eb6e6454dbb0342850e94ce8cf2b9709f4a1a08c
jeyroik <jeyroik@gmail.com>
2022-05-18 12:50:32 +0300

    added array access methods

--- src/components/TCanBeAsArray.php
+++ src/components/TCanBeAsArray.php
@@ -12,4 +12,113 @@ trait TCanBeAsArray
     {
         return $this->attributes ?? [];
     }
+
+    /**
+     * @param $name
+     * @return bool
+     */
+    public function __isset($name)
+    {
+        return isset($this->config[$name]);
+    }
+
+    /**
+     * @var int
+     */
+    protected int $currentKey = 0;
+
+    /**
+     * @var array
+     */
+    protected array $keyMap = [];
+
+    /**
+     * @param array $data
+     * @return $this
+     */
+    public function __merge(array $data)
+    {
+        foreach ($data as $key => $value) {
+            $this->attributes[$key] = $value;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $offset
+     *
+     * @return bool
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->attributes[$offset]);
+    }
+
+    /**
+     * @param mixed $offset
+     *
+     * @return mixed|null
+     */
+    public function offsetGet($offset)
+    {
+        return $this->attributes[$offset] ?? null;
+    }
+
+    /**
+     * @param mixed $offset
+     * @param mixed $value
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->attributes[$offset] = $value;
+    }
+
+    /**
+     * @param mixed $offset
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->attributes[$offset]);
+    }
+
+    /**
+     * @return bool
+     */
+    public function valid()
+    {
+        return isset($this->keyMap[$this->currentKey]);
+    }
+
+    /**
+     * @return string|null
+     */
+    public function key()
+    {
+        return $this->keyMap[$this->currentKey] ?? null;
+    }
+
+    /**
+     * @return void
+     */
+    public function next()
+    {
+        $this->currentKey++;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function current()
+    {
+        return $this->attributes[$this->keyMap[$this->currentKey]];
+    }
+
+    /**
+     * @return void
+     */
+    public function rewind()
+    {
+        $this->currentKey = 0;
+    }
 }
